21747d0013f959217e2126f58498c626e3b3b775
NikolayA <nklya@hotmail.com>
2016-03-08 21:04:04 +0300

    second step 2

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def hello (environ, start_response):
+def application (environ, start_response):
   status = '200 OK'
   response_headers = [('Content-type','text/plain')]
   start_response(status, response_headers)
