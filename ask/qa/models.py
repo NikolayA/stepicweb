515940c1f8901435cc0de24cf400f1879f23fd01
NikolayA <nklya@hotmail.com>
2016-03-16 20:35:10 +0300

    Django3_5

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,36 +1,21 @@
 from django.db import models
-from django.core.urlresolvers import reverse
-#from django.contrib.auth.models import User
-
-
+from django.contrib.auth.models import User
 # Create your models here.
-class Question(models.Model):
-  title = models.CharField(max_length=255)
-  text = models.TextField()
-  added_at = models.DateTimeField(auto_now_add=True)
-  rating = models.IntegerField(default=0)
-  author = models.ForeignKey(User,db_constraint=False, default=1)
-  likes = models.ManyToManyField(User, related_name='likes_')
-  def __str__(self):
-        return self.title
-  def get_url(self):
-        return 'http://0.0.0.0/question/' + str(self.id) + '/' 
 
-class Answer(models.Model):
-  text = models.TextField()
-  added_at = models.DateTimeField(auto_now_add=True)
-  question = models.ForeignKey(Question)
-  author = models.ForeignKey(User, db_constraint=False)
-  def __str__(self):
-        return self.text
+class Question(models.Model) :
+	#id = models.IntegerField(primary_key=True)
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User, related_name='+', default=1)
+	likes = models.ManyToManyField(User, default=1)
 
-class User(models.Model):
-    login = models.CharField(unique=True)
-    password = models.CharField()
-    email = models.EmailField()
+	def get_absolute_url(self) :
+		return '/question/%d/' % self.pk
 
-class Session(models.Model):
-    key = models.CharField(unique=True)
-    user = models.ForeignKey(User)
-    expires = models.DateTimeField()
-
+class Answer(models.Model) :
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question, null=False, on_delete=models.DO_NOTHING)
+	author = models.ForeignKey(User, related_name='+', default=1)
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,92 +1,145 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
-from django.http import HttpResponse, HttpResponseRedirect
-from models import Question, Answer
-from forms import AnswerForm, AskForm
-from django.views.decorators.csrf import csrf_protect
-from django.template import RequestContext
-from django.core.context_processors import csrf
-#import django.middleware.csrf.CsrfViewMiddleware
-
-# Create your views here.
-
-
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from qa.models import Question, Answer
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from qa.forms import AskForm, AnswerForm, SignupForm, LoginForm
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
-def qa_all(request, *args, **kwargs):
-    questions = Question.objects.filter()
-    questions = questions.order_by('-id')
-    limit = request.GET.get('limit', 10)
-    page = request.GET.get('page', 1)
-    paginator = Paginator(questions, limit)
-    paginator.baseurl = '/?page='
-    page = paginator.page(page)
-    return render(request, 'qa_last.html', {
-        'questions': page.object_list,
-        'paginator': paginator, 'page': page,
-    })
+def index(request) :
+	pageLimit = 10
+	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
+	qwests = Question.objects.all().order_by('-id')
+	
+	from django.core.paginator import Paginator
+	
+	page = request.GET.get('page') or 1
+	try :
+		page = int(page)
+	except ValueError :
+		page = 1
+	paginator = Paginator(qwests, pageLimit)
+	paginator.baseurl = '/?page='
+	try :
+		page = paginator.page(page)
+	except EmptyPage :
+		page = paginator.page(paginator.num_pages)
+	return render(request, 'questionList.html', {
+		'title' : 'qwests and answers',
+		'list' : page.object_list,
+		'paginator' : paginator, 
+		'page' : page,
+	})
+	
 
-def popular(request, *args, **kwargs):
-    questions = Question.objects.filter()
-    questions = questions.order_by('-rating')
-    limit = request.GET.get('limit', 10)
-    page = request.GET.get('page', 1)
-    paginator = Paginator(questions, limit)
-    paginator.baseurl = '/?page='
-    page = paginator.page(page)
-    return render(request, 'qa_popular.html', {
-        'questions': page.object_list,
-        'paginator': paginator, 'page': page,
-    })
+def popular(request) :
+	pageLimit = 10
+	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
+	qwests = Question.objects.all().order_by('-likes')
+	
+	from django.core.paginator import Paginator
+	
+	page = request.GET.get('page') or 1
+	try :
+		page = int(page)
+	except ValueError :
+		page = 1
+	paginator = Paginator(qwests, pageLimit)
+	paginator.baseurl = '/?page='
+	try :
+		page = paginator.page(page)
+	except EmptyPage :
+		page = paginator.page(paginator.num_pages)
+	return render(request, 'questionList.html', {
+		'title' : 'popular quests',
+		'list' : page.object_list,
+		'paginator' : paginator, 
+		'page' : page,
+	})
 
-@csrf_protect
-def qa_details(request, qid):
-    question = get_object_or_404(Question, id=qid)
-    answers = Answer.objects.filter(question_id=qid)
-    form = AnswerForm()
+def question(request, quest_id) :
+	try :
+		quest = Question.objects.get(id = quest_id)
+	except Question.DoesNotExist :
+		raise Http404
+	answers = Answer.objects.all().filter(question = quest)
+	
+	title = 'qwest ' + quest_id
+	
+	user = request.user
+	
+	if user.is_authenticated() :
+		form = AnswerForm(initial={'question' : quest_id})
+	
+	return render(request, 'question.html', {
+		'title' : title,
+		'question' : quest,
+		'list' : answers,
+		'form' : form,
+	})
 
-    return render(request, 'qa_details.html', {
-        'title': question.title,
-        'qtext': question.text,
-        'answers':answers,
-        'form':form,
-    })
-
-@csrf_protect
-def ask(request):
-    if request.method == 'POST':
-        form = AskForm(request.POST)
-        if form.is_valid():
-            question = form.save()
-            url = question.get_url()
-            return HttpResponseRedirect(url)
-    else:
-        form = AskForm()
-    return render(request, 'ask.html',{
-            'form':form,
-        })
-
-@csrf_protect
-def answer(request):
-    if request.method == 'POST':
-        form = AnswerForm(request.POST)
-        if form.is_valid():
-            answer = form.save()
-            question = answer.question
-            url = question.get_url()
-            return HttpResponseRedirect(url)
-    else:
-        form = AnswerForm()
-    return render(request, 'answer.html',{
-            'form':form,
-        })
-
-@csrf_protect
-def login(request):
-    return HttpResponse('OK')
-
-@csrf_protect
-def signup(request):
-    return HttpResponse('OK')
+def ask(request) :
+	user = request.user
+	if not user.is_authenticated():
+		raise Http404
+	if request.method == "POST" :
+		#print("POST!!!!!!!!!!!!!!!!!!!!!!!!!")
+		form = AskForm(request.POST)
+		if form.is_valid():
+			#print("FORM IS VALID!!!!!!!!!!!!")
+			form.author = user
+			quest = form.save()
+			#print("QUEST IS CREATE!!!!!!!!!!")
+			url = quest.get_absolute_url()
+			#print("URL = " + url +"!!!!!!!!!")
+			return HttpResponseRedirect(url)
+	else :
+		form = AskForm()
+	return render(request, 'ask_add.html', {
+		'form' : form
+	})
+	
+def answer(request) :
+	if request.method == "POST" :
+		form = AnswerForm(request.POST)
+		if form.is_valid():
+			answer = form.save()
+			url = '/question/' + form.question
+			return HttpResponseRedirect(url)
+			
+def signup(request) :
+	if request.method == "POST" :
+		form = SignupForm(request.POST)
+		if form.is_valid() :
+			print("POST!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+			print("username: " + request.POST.get("username") + "!")
+			print("password: " + request.POST.get("password") + "!")
+			print("email: " + request.POST.get("email") + "!")
+			form.set_password(request.POST.get("password"))
+			user = form.save()
+			print("SAVE USER!!!!!!!!!!!!!!!!!!!!!!!")
+			print("username: " + user.username + " !")
+			print("password: " + user.password + " !")
+			print("email: " + user.email + " !")
+			form.loginUser(request)
+			return HttpResponseRedirect("/")
+	else :
+		form = SignupForm()
+	return render(request, 'ask_add.html', {
+		'form' : form,
+	})
+	
+def login(request) :
+	if request.method == "POST" :
+		form = LoginForm(request.POST)
+		if form.is_valid() :
+			form.set_password(request.POST.get("password"))
+			user = form.loginUser(request)
+			return HttpResponseRedirect("/")
+	else :
+		form = LoginForm()
+	return render(request, 'ask_add.html', {
+		'form' : form,
+	})
