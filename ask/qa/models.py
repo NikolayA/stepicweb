b5ab29eb0063f8243fccca69228e678d0e2578f6
NikolayA <nklya@hotmail.com>
2016-03-16 19:52:41 +0300

    Django3_3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,17 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+#from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Question(models.Model):
-  title = models.CharField()
+  title = models.CharField(max_length=255)
   text = models.TextField()
-  added_at = models.DateTimeField()
-  rating = models.IntegerField()
-  author = models.ForeignKey(User)
-  likes = models.ManyToManyField(User, related_name='likes_') 
+  added_at = models.DateTimeField(auto_now_add=True)
+  rating = models.IntegerField(default=0)
+  author = models.ForeignKey(User,db_constraint=False, default=1)
+  likes = models.ManyToManyField(User, related_name='likes_')
+  def __str__(self):
+        return self.title
+  def get_url(self):
+        return 'http://0.0.0.0/question/' + str(self.id) + '/' 
 
 class Answer(models.Model):
   text = models.TextField()
-  added_at = models.DateTimeField()
+  added_at = models.DateTimeField(auto_now_add=True)
   question = models.ForeignKey(Question)
-  author = models.ForeignKey(User)
+  author = models.ForeignKey(User, db_constraint=False)
+  def __str__(self):
+        return self.text
+
+class User(models.Model):
+    login = models.CharField(unique=True)
+    password = models.CharField()
+    email = models.EmailField()
+
+class Session(models.Model):
+    key = models.CharField(unique=True)
+    user = models.ForeignKey(User)
+    expires = models.DateTimeField()
+
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,92 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.http import HttpResponse, HttpResponseRedirect
+from models import Question, Answer
+from forms import AnswerForm, AskForm
+from django.views.decorators.csrf import csrf_protect
+from django.template import RequestContext
+from django.core.context_processors import csrf
+#import django.middleware.csrf.CsrfViewMiddleware
 
 # Create your views here.
+
+
+
 def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def qa_all(request, *args, **kwargs):
+    questions = Question.objects.filter()
+    questions = questions.order_by('-id')
+    limit = request.GET.get('limit', 10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(questions, limit)
+    paginator.baseurl = '/?page='
+    page = paginator.page(page)
+    return render(request, 'qa_last.html', {
+        'questions': page.object_list,
+        'paginator': paginator, 'page': page,
+    })
+
+def popular(request, *args, **kwargs):
+    questions = Question.objects.filter()
+    questions = questions.order_by('-rating')
+    limit = request.GET.get('limit', 10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(questions, limit)
+    paginator.baseurl = '/?page='
+    page = paginator.page(page)
+    return render(request, 'qa_popular.html', {
+        'questions': page.object_list,
+        'paginator': paginator, 'page': page,
+    })
+
+@csrf_protect
+def qa_details(request, qid):
+    question = get_object_or_404(Question, id=qid)
+    answers = Answer.objects.filter(question_id=qid)
+    form = AnswerForm()
+
+    return render(request, 'qa_details.html', {
+        'title': question.title,
+        'qtext': question.text,
+        'answers':answers,
+        'form':form,
+    })
+
+@csrf_protect
+def ask(request):
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+        if form.is_valid():
+            question = form.save()
+            url = question.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AskForm()
+    return render(request, 'ask.html',{
+            'form':form,
+        })
+
+@csrf_protect
+def answer(request):
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save()
+            question = answer.question
+            url = question.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AnswerForm()
+    return render(request, 'answer.html',{
+            'form':form,
+        })
+
+@csrf_protect
+def login(request):
+    return HttpResponse('OK')
+
+@csrf_protect
+def signup(request):
     return HttpResponse('OK')
