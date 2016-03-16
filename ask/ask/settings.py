3e54eb88314771fcc0561a1e65dc6bdb5dd6d4ae
NikolayA <nklya@hotmail.com>
2016-03-16 18:44:17 +0300

    Django3_1

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -55,23 +55,23 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-DATABASES = {                                                                   
-  'default': {                                                                  
-        'ENGINE': 'django.db.backends.mysql',                                   
-        'NAME': 'stepicweb',                                                    
-        'USER': 'stepic',                                                       
-        'PASSWORD': 'superpassword',                                            
-        'HOST': 'localhost',                                                    
-        'PORT': '',                                                             
-   }                                                                            
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+#DATABASES = {                                                                   
+#  'default': {                                                                  
+#        'ENGINE': 'django.db.backends.mysql',                                   
+#        'NAME': 'stepicweb',                                                    
+#        'USER': 'stepic',                                                       
+#        'PASSWORD': 'superpassword',                                            
+#        'HOST': 'localhost',                                                    
+#        'PORT': '',                                                             
+#   }                                                                            
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
